25bd9737e5493779d28e7587d7960f0d52690540
Mr.Ülg3n <68772806+SwazzAtena@users.noreply.github.com>
2023-09-09 16:18:32 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir sayı girin: ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+
+            long faktoriyel = FaktoriyelHesapla(sayi);
+            Console.WriteLine($"{sayi}! = {faktoriyel}");
+
+            Console.ReadLine(); // Kullanıcının çıktıyı görmesi için bir tuşa basmasını bekleyin.
+        }
+
+        static long FaktoriyelHesapla(int n)
+        {
+            if (n == 0)
+                return 1;
+            else
+                return n * FaktoriyelHesapla(n - 1);
+        }
+    }
+}
